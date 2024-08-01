edcf68fdc17249f304236584ba7d3331f4269767
Mehni <Mehni@users.noreply.github.com>
2024-08-01 17:32:58 +0200

    fixed nullpointer for immature dryads Fixes #80

--- Numbers/PawnColumnWorkers/PawnColumnWorker_FoodConsumption.cs
+++ Numbers/PawnColumnWorkers/PawnColumnWorker_FoodConsumption.cs
@@ -6,7 +6,7 @@ namespace Numbers
     public class PawnColumnWorker_FoodConsumption : PawnColumnWorker_Text
     {
         protected override string GetTextFor(Pawn pawn)
-            => RaceProperties.NutritionEatenPerDay(pawn);
+            => (pawn?.needs?.food?.FoodFallPerTickAssumingCategory(HungerCategory.Fed) * 60000f ?? 0).ToString("0.##");
 
         public override int Compare(Pawn a, Pawn b)
             => (a?.needs?.food?.FoodFallPerTickAssumingCategory(HungerCategory.Fed) * 60000f ?? 0)
