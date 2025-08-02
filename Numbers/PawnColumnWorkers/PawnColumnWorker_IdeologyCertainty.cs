25013ed80aec1cd37fdc006040a24f4535e586fd
BloockyPixels <siddharth_manthan@protonmail.com>
2025-08-03 02:12:48 +0530

    Add Ideology Certainty

---
+++ Numbers/PawnColumnWorkers/PawnColumnWorker_IdeologyCertainty.cs
@@ -0,0 +1,33 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Numbers
+{
+    public class PawnColumnWorker_IdeologyCertainty : PawnColumnWorker_Text
+    {
+        protected override string GetTextFor(Pawn pawn)
+        {
+            float? certainty = pawn.ideo?.Certainty;
+            return certainty is float value && value >= 0f
+                ? $"{value * 100f:F0}%"
+                : "-";
+        }
+
+        protected override string GetTip(Pawn pawn)
+        {
+            string ideoName = pawn.ideo?.Ideo?.name ?? "None";
+            string certainty = GetTextFor(pawn);
+            return $"Certainty in {ideoName}: {certainty}";
+        }
+
+        protected override string GetHeaderTip(PawnTable table)
+            => "The Certainty of Pawn in Ideology.";
+
+        public override int Compare(Pawn a, Pawn b)
+        {
+            float certaintyA = a.ideo?.Certainty ?? -1f;
+            float certaintyB = b.ideo?.Certainty ?? -1f;
+            return certaintyA.CompareTo(certaintyB);
+        }
+    }
+}
