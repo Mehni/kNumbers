49167831ab74cec7c2173c1974db2b1881e84a85
BloockyPixels <siddharth_manthan@protonmail.com>
2025-08-18 12:39:52 +0530

    Unwavering Loyalty now visible in all Equipment Bearers

---
+++ Numbers/PawnColumnWorkers/PawnColumnWorker_Loyalty.cs
@@ -0,0 +1,28 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Numbers
+{
+    public class PawnColumnWorker_Loyalty : PawnColumnWorker_Text
+    {
+        protected override string GetTextFor(Pawn pawn)
+        {
+            if (pawn.guest == null) return string.Empty;
+
+            return pawn.guest.Recruitable ? "UnwaveringlyLoyal".Translate() : "Recruitable".Translate();
+        }
+
+        public override int Compare(Pawn a, Pawn b)
+        {
+            bool aUnwavering = a.guest?.Recruitable == true;
+            bool bUnwavering = b.guest?.Recruitable == true;
+
+            return aUnwavering.CompareTo(bUnwavering);
+        }
+
+        protected override string GetTip(Pawn pawn)
+        {
+            return "UnwaveringlyLoyalDesc".Translate();
+        }
+    }
+}
